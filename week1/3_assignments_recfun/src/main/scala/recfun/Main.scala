0c484e5ac069d317777e2ad40e0c6dec223fe88c
unknown <mlmail2121@gmail.com>
2019-07-30 22:22:21 +0300

    week1(RecursionAssignment): implement exercise 1
    : https://www.coursera.org/learn/progfun1/programming/Ey6Jf/recursion

---
+++ week1/3_assignments_recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,34 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      def isFirstElement: Boolean = c == 0 && r ==0
+      def isOutsideElement: Boolean = (c < 0 ||  c > r + 1) || (r < 0)
+
+      if(isFirstElement) 1
+      else if (isOutsideElement) 0
+      else pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
